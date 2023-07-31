9b3ad4b982123634d8ceca05dee1cc60149cf255
Nuruddin-Adnan <contact.codepixel@gmail.com>
2023-07-31 23:13:22 +0600

    cart done

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Button } from '@/components/ui/button';
 import { DatePickerWithPresets } from '@/components/ui/datePickerWithPreset';
 import { Input } from '@/components/ui/input';
@@ -5,7 +6,7 @@ import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Switch } from '@/components/ui/switch';
 import { Textarea } from '@/components/ui/textarea';
-import { IProduct } from '@/types/globalTypes';
+import { useAppSelector } from '@/redux/hook';
 
 import { useState } from 'react';
 
@@ -14,7 +15,7 @@ export default function Checkout() {
 
   //! Dummy Data
 
-  const products: IProduct[] = [];
+  const { products, total } = useAppSelector((state) => state.cart);
 
   //! **
 
@@ -121,7 +122,7 @@ export default function Checkout() {
           <div className="space-y-2">
             <div className="flex justify-between text-lg">
               <p>Subtotal</p>
-              <p>77.90$</p>
+              <p>{total.toFixed(2)}$</p>
             </div>
             <div className="flex justify-between text-lg">
               <p>Delivery</p>
@@ -129,7 +130,7 @@ export default function Checkout() {
             </div>
             <div className="flex justify-between text-xl font-bold">
               <p>Total</p>
-              <p>81.95$</p>
+              <p>{(total + 4.5).toFixed(2)}$</p>
             </div>
             <Button className="w-full">Checkout</Button>
           </div>
